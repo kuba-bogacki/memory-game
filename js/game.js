6377f048573ce19330502dd897283721a29cd7d4
justyna-eevee <justyna.zaczyk@onet.pl>
2022-01-11 11:59:32 +0100

    create function lose_with_timeout

--- js/game.js
+++ js/game.js
@@ -1,17 +1,27 @@
+let time = 0;
+let timeCounter;
+let lose;
+
+
 initGame();
 
-let time = 0
-
 function initGame() {
-
-    // Your game can start here, but define separate functions, don't write everything in here :)
+    const queryString = window.location.search; // zwraca cały url
+    const urlParams = new URLSearchParams(queryString); // odcina to co jest po ? wyciąga klucze i wartości
+    const is_time = urlParams.get('is_time'); // wyciągam konkretną wartość
+    const level = urlParams.get('level_of_difficulty'); // wyciągam konkretną wartość
+    if (is_time === "yes"){
+        create_time(level);
+    }
     timeCounter = setInterval(decrease_time, 1000);
+    lose = setInterval(lose_with_timeout, 1000);
+
 
 }
 
 function create_time(level){
     if (level === "easy"){
-        time = 60;
+        time = 3;
     }
     else if (level === "normal"){
         time = 300;
@@ -23,7 +33,7 @@ function create_time(level){
 
 function decrease_time(){
     time -= 1;
-    document.getElementById('time_box').value = time;
+    document.getElementsByClassName('time_box')[0].innerHTML = "<h3>Time left: " + time + "</h3>";
 }
 
 function win_with_timeout(){
@@ -35,5 +45,10 @@ function win_without_timeout(){
 }
 
 function lose_with_timeout(){
-
+    if (time === 0){
+        clearInterval(timeCounter);
+        clearInterval(lose);
+        alert('TIME IS OVER');
+        location.href = 'index.html';
+    }
 }
